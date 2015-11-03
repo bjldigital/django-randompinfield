1ea9f87a94faebe97e8acf5859c6a1d471b22fad
Danny Wilson <dannywilson32@gmail.com>
2015-11-03 17:12:43 +0000

    Bump version: 0.4.4 → 0.4.5

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ f = open('README.rst', 'r')
 setup(
   name = 'django-randompinfield',
   packages = ['randompinfield'],
-  version = '0.4.4',
+  version = '0.4.5',
   license="MIT",
   description = 'A django model field that generates a random pin of a desired length',
   long_description=f.read(),
   author = 'Danny Wilson',
   author_email = 'dannywilson32@gmail.com',
   url = 'https://github.com/bjldigital/django-randompinfield/',
-  download_url = 'https://github.com/bjldigital/django-randompinfield/archive/v0.4.4.tar.gz',
+  download_url = 'https://github.com/bjldigital/django-randompinfield/archive/v0.4.5.tar.gz',
   keywords = ['django', 'model', 'field'],
   classifiers = [
     "Framework :: Django",
