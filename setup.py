6cf6113e302af83f1126a50a4a48f8e4c9ef6d31
Danny Wilson <dannywilson32@gmail.com>
2015-11-03 16:14:21 +0000

    updated the way setup reads the readme

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from distutils.core import setup
 
-with open('README.rst') as f:
-    readme = f.read()
+f = open('README.rst', 'r')
 
 setup(
   name = 'django-randompinfield',
   packages = ['randompinfield'],
-  version = '0.4.3',
+  version = '0.4.4',
   license="MIT",
   description = 'A django model field that generates a random pin of a desired length',
-  long_description=readme,
+  long_description=f.read(),
   author = 'Danny Wilson',
   author_email = 'dannywilson32@gmail.com',
   url = 'https://github.com/bjldigital/django-randompinfield/',
-  download_url = 'https://github.com/bjldigital/django-randompinfield/archive/v0.4.3.tar.gz',
+  download_url = 'https://github.com/bjldigital/django-randompinfield/archive/v0.4.4.tar.gz',
   keywords = ['django', 'model', 'field'],
   classifiers = [
     "Framework :: Django",
